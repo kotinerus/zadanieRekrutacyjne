009daa80c6f42d1b0043202cfe36a50bc00217c0
Krzysztof Wysocki <kotinerus@gmail.com>
2023-08-16 00:04:38 +0200

    Photos gallery

---
+++ main.js
@@ -0,0 +1,13 @@
+var myInstance = new Macy({
+  container: ".macy",
+  trueOrder: true,
+  waitForImages: false,
+  margin: 50,
+  columns: 3,
+  breakAt: {
+    1120: 3,
+    992: 3,
+    768: 2,
+    480: 1,
+  },
+});
