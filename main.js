8b357428a158eb071413030a348b90a1a97bca8d
Krzysztof Wysocki <kotinerus@gmail.com>
2023-08-17 23:45:14 +0200

    Responsive 6/6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var myInstance = new Macy({
+let myInstance = new Macy({
   container: ".macy",
   trueOrder: true,
   waitForImages: false,
@@ -8,3 +8,25 @@ var myInstance = new Macy({
     1200: 1,
   },
 });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#leftArrow",
+    prevEl: "#rightArrow",
+  },
+});
+
+// elemenet.textContent = `<a id="rightArrow" class="btn d-flex border-none align-items-center justify-content-center col-6 "><img src="./icons/arrowLeft.svg"></a>
+// <a id="leftArrow" class="btn d-flex border-none align-items-center justify-content-center col-6 "><img src="./icons/arrowRight.svg"></a>`;
+
+// const swipper = document.querySelectorAll(".swiper-slide");
+// for (let index = 0; index < swipper.length; index++) {
+//   console.log(elemenet);
+//   swipper[index].appendChild(elemenet);
+//   console.log(swipper[index]);
+// }
