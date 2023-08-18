6f457723eea24dfdc2888d9ceeaa244d78316ab1
Krzysztof Wysocki <kotinerus@gmail.com>
2023-08-18 17:14:00 +0200

    Cleaning code 1.0

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let myInstance = new Macy({
+const myInstance = new Macy({
   container: ".macy",
   trueOrder: true,
   waitForImages: false,
@@ -10,13 +10,39 @@ let myInstance = new Macy({
 });
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+const endProductArrowDown = document.querySelector("#endProduct_arrowDown");
+const gradient = document.querySelector(".gradient_top");
+const endProductContainer = document.querySelector("#endProduct");
+const macyContainer = document.querySelector(".macy");
+const welcomeContainerFluid = document.querySelector("#welcomeContainer");
+const welcomeContainer = endProductArrowDown.addEventListener("click", (e) => {
+  gradient.classList.add("d-none");
+  endProductContainer.classList.add("fit-content");
+  macyContainer.classList.remove("h-75");
+});
+
+window.addEventListener("load", () => {
+  welcomeContainerFluid.style.left = "0% ";
+  welcomeContainerFluid.style.transition = "0.7s";
+});
+let options = {
+  root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 0.5,
+  isIntersecting: true,
+};
+
+let observer = new IntersectionObserver((e) => {});
+
+const elementsToObserve = Array.from(document.querySelectorAll(".animate"));
+elementsToObserve.forEach((element) => {
+  observer.observe(element);
 });
